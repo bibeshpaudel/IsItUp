c1c7b949e3c1d9b5e2996ad12c89668ed59282ce
bibesh <bibesh03@gmail.com>
2026-04-15 00:11:52 +0545

    base added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import fs from 'fs';
 
 function generateVersion() {
@@ -15,10 +15,11 @@ function generateVersion() {
   };
 }
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss(), generateVersion()],
+    base: '/IsItUp/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
